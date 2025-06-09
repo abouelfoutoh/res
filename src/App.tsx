c24afaad9a344f45ffb761cb5293d3ed6aaace91
Mohamed Abouelfoutoh <mhmdapady@gmail.com>
2025-06-09 07:54:06 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/login" element={<Login />} />
-            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/" element={<Navigate to="/dashboard\" replace />} />
             
             {/* Protected Routes */}
             <Route path="/dashboard" element={
